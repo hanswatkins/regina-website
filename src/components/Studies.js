858f06349cf57a38be5a5e78c725f65acecdd875
hanswatkins <hanswatkins@gmail.com>
2022-07-24 21:42:31 -0700

    changed studies formatting

--- src/components/Studies.js
+++ src/components/Studies.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Studies = () => {
     return (
         <div className='studies-body'>
-            <h1>A Title, Perhaps?</h1>
+            <h2 className='studies-h2'>A Title, Perhaps?</h2>
             <p className='studies-p'>This is where you can post pictures and talk about your studies. Maybe something awesome happened that you can share you experience with. And then, you wrap it up with an excellent lesson you learned and move on to the next one!</p>
 
-            <h1>What's This, Another Title?</h1>
+            <h2 className='studies-h2'>What's This, Another Title?</h2>
             <p className='studies-p'>This is where you can post pictures and talk about your studies. Maybe something awesome happened that you can share you experience with. And then, you wrap it up with an excellent lesson you learned and move on to the next one!</p> 
 
-            <h1>Alas! Yet Another Title!</h1>
+            <h2 className='studies-h2'>Alas! Yet Another Title!</h2>
             <p className='studies-p'>This is where you can post pictures and talk about your studies. Maybe something awesome happened that you can share you experience with. And then, you wrap it up with an excellent lesson you learned and move on to the next one!</p> 
         </div>
     );
